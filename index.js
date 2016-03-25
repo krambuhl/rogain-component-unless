504ebd40dc1cfab32c6a5743663046dff92c514a
Evan Krambuhl <thekrambuhl@gmail.com>
2016-03-25 00:35:29 -0700

    wrong split

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Empty = require('rogain-component-empty');
 const splitTrees = require('rogain-utils').splitTrees;
+
 module.exports = function Unless(tree, props) {
   if (!tree.attrs.value) {
     return Empty(tree, props);
   }
 
-  var split = splitTrees(tree, { type: 'component', name: 'Else' });
+  var split = splitTrees(tree.children, { type: 'component', name: 'Else' });
   var res = split[1];
   
   if (tree.data != tree.attrs.value) {
